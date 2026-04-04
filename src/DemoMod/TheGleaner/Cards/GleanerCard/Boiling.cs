695481fa7555dc369ff3fcb2ab54d4416b3a4a17
AquaMeow285714 <aquapatate@gmail.com>
2026-04-04 21:09:34 +0800

    修改Boiling

--- src/DemoMod/TheGleaner/Cards/GleanerCard/Boiling.cs
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/Boiling.cs
@@ -6,50 +6,36 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
-using System.Linq;
 
 namespace DemoMod.TheGleaner.Cards.GleanerCard;
 
 [Pool(typeof(CardPool))]
-public class Boiling : CustomCardModel {
+public class Boiling : CustomCardModel
+{
     public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
-
-    private readonly List<CardModel> _affectedCards = [];
-    private bool _costIncreaseActive;
 
     protected override IEnumerable<DynamicVar> CanonicalVars => [
         new EnergyVar("EnergyAmount", 2),
         new EnergyVar("Amount", 1)
     ];
 
-    public Boiling() : base(0, CardType.Skill, CardRarity.Uncommon, TargetType.Self) {
+    public Boiling() : base(0, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+    {
     }
 
-    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
         await PlayerCmd.GainEnergy(DynamicVars["EnergyAmount"].BaseValue, Owner);
 
-        _affectedCards.Clear();
-        foreach (CardModel card in Owner.PlayerCombatState.Hand.Cards.ToList()) {
-            card.EnergyCost.AddThisCombat(1);
-            _affectedCards.Add(card);
+        List<CardModel> handCards = PileType.Hand.GetPile(Owner).Cards.ToList();
+        foreach (CardModel card in handCards)
+        {
+            card.EnergyCost.AddUntilPlayed((int)DynamicVars["Amount"].BaseValue, false);
         }
-
-        _costIncreaseActive = _affectedCards.Count > 0;
     }
 
-    public override async Task AfterCardPlayed(PlayerChoiceContext context, CardPlay cardPlay) {
-        if (!_costIncreaseActive || cardPlay.Card.Owner != Owner || cardPlay.Card.Type != CardType.Attack) {
-            return;
-        }
-
-        foreach (CardModel card in _affectedCards.Where(c => c.CombatState != null)) {
-            card.EnergyCost.AddThisCombat(-1);
-        }
-
-        _affectedCards.Clear();
-        _costIncreaseActive = false;
-        await Task.CompletedTask;
+    protected override void OnUpgrade()
+    {
+        DynamicVars["EnergyAmount"].UpgradeValueBy(1);
     }
-
-    protected override void OnUpgrade() => DynamicVars["EnergyAmount"].UpgradeValueBy(1);
 }
