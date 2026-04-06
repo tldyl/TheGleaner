c5aca87e98e4fad8071a90f8c2ae11bfbc398e77
AquaMeow285714 <aquapatate@gmail.com>
2026-04-06 12:00:31 +0800

    新增两张牌，修改呢喃奏

--- src/DemoMod/TheGleaner/Cards/GleanerCard/Bisbigliando.cs
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/Bisbigliando.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BaseLib.Abstracts;
+using BaseLib.Utils;
+using DemoMod.TheGleaner.Commands;
 using DemoMod.TheGleaner.Pools;
 using Godot;
 using MegaCrit.Sts2.Core.Combat;
@@ -12,16 +14,12 @@ using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Nodes.Cards;
 using MegaCrit.Sts2.Core.Nodes.Rooms;
 using MegaCrit.Sts2.Core.Saves;
 using MegaCrit.Sts2.Core.Settings;
 using MegaCrit.Sts2.Core.ValueProps;
-using BaseLib.Utils;
-using DemoMod.TheGleaner.Commands;
-using MegaCrit.Sts2.Core.Models;
-
-
 
 namespace DemoMod.TheGleaner.Cards.GleanerCard;
 
@@ -31,13 +29,7 @@ public class Bisbigliando : CustomCardModel
     public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
 
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new CalculationBaseVar(5),
-        new ExtraDamageVar(1),
-        new CalculatedDamageVar(ValueProp.Move).WithMultiplier((CardModel card, Creature _) =>
-            -CombatManager.Instance.History.CardPlaysFinished.Count((CardPlayFinishedEntry e) =>
-                e.HappenedThisTurn(card.CombatState)
-                && e.CardPlay.Card.Id.Entry == card.Id.Entry
-                && e.CardPlay.Card.Owner == card.Owner))
+        new DamageVar(5, ValueProp.Move)
     ];
 
     public Bisbigliando() : base(0, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
@@ -46,29 +38,42 @@ public class Bisbigliando : CustomCardModel
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await DamageCmd.Attack(DynamicVars.CalculatedDamage)
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
             .FromCard(this)
             .Targeting(cardPlay.Target)
             .Execute(choiceContext);
 
-        await ScorePileCmd.AddCards(Owner.PlayerCombatState, Owner, this);
+        if (!HasBeenPlayedThisTurn) {
+            await ScorePileCmd.AddCards(Owner.PlayerCombatState, Owner, this);
 
-        NCard cardNode = NCombatRoom.Instance?.Ui.GetCardFromPlayContainer(this);
-        if (cardNode != null)
-        {
-            Tween tween = NCombatRoom.Instance.CreateTween().SetParallel();
-            tween.Parallel().TweenProperty(
-                cardNode,
-                (NodePath)"modulate",
-                StsColors.exhaustGray,
-                SaveManager.Instance.PrefsSave.FastMode == FastModeType.Fast ? 0.2 : 0.3
-            );
-            tween.Chain().TweenCallback(Callable.From(cardNode.QueueFreeSafely));
+            NCard cardNode = NCombatRoom.Instance?.Ui.GetCardFromPlayContainer(this);
+            if (cardNode != null) {
+                Tween tween = NCombatRoom.Instance.CreateTween().SetParallel();
+                tween.Parallel().TweenProperty(
+                    cardNode,
+                    (NodePath)"modulate",
+                    StsColors.exhaustGray,
+                    SaveManager.Instance.PrefsSave.FastMode == FastModeType.Fast ? 0.2 : 0.3
+                );
+                tween.Chain().TweenCallback(Callable.From(cardNode.QueueFreeSafely));
+            }
         }
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.CalculationBase.UpgradeValueBy(2);
+        DynamicVars.Damage.UpgradeValueBy(2);
+    }
+
+    private bool HasBeenPlayedThisTurn
+    {
+        get
+        {
+            return CombatManager.Instance.History.CardPlaysFinished.Any(
+                (CardPlayFinishedEntry e) =>
+                    e.CardPlay.Card == this &&
+                    e.HappenedThisTurn(CombatState)
+            );
+        }
     }
 }
