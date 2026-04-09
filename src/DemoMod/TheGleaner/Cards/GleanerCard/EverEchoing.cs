0710d82330916f9c764d710843eccf2ad2356fb1
tldyl <756560020@qq.com>
2026-04-09 13:13:32 +0800

    新增2张牌

---
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/EverEchoing.cs
@@ -0,0 +1,40 @@
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using DemoMod.TheGleaner.CardPiles;
+using DemoMod.TheGleaner.Commands;
+using DemoMod.TheGleaner.Pools;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Models;
+
+namespace DemoMod.TheGleaner.Cards.GleanerCard;
+
+[Pool(typeof(CardPool))]
+public class EverEchoing : CustomCardModel {
+    //public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        CardKeyword.Exhaust
+    ];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.ReplayStatic)];
+    protected override bool HasEnergyCostX => true;
+
+    public EverEchoing() : base(0, CardType.Skill, CardRarity.Rare, TargetType.Self) {
+        
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+        ScorePile scorePile = ScorePileCmd.GetOrCreateScorePile(Owner.PlayerCombatState);
+        CardSelectorPrefs prefs = new CardSelectorPrefs(new LocString("cards", "DEMOMOD-EVER_ECHOING.selectionScreenPrompt"), 1);
+        CardModel selectedCard = (await CardSelectCmd.FromSimpleGrid(choiceContext, scorePile.Cards.ToList(), Owner, prefs)).FirstOrDefault();
+        if (selectedCard != null) {
+            selectedCard.BaseReplayCount += ResolveEnergyXValue() + CurrentUpgradeLevel;
+            if (!selectedCard.Keywords.Contains(CardKeyword.Exhaust)) {
+                selectedCard.AddKeyword(CardKeyword.Exhaust);
+            }
+        }
+    }
+}
---
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/RendezvousWithDoom.cs
@@ -0,0 +1,27 @@
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using DemoMod.TheGleaner.Pools;
+using DemoMod.TheGleaner.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace DemoMod.TheGleaner.Cards.GleanerCard;
+
+[Pool(typeof(CardPool))]
+public class RendezvousWithDoom : CustomCardModel {
+    //public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.Energy)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1)];
+    protected override bool HasEnergyCostX => true;
+
+    public RendezvousWithDoom() : base(0, CardType.Power, CardRarity.Rare, TargetType.Self) {
+        
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+        await PowerCmd.Apply<RendezvousWithDoomPower>(Owner.Creature, ResolveEnergyXValue() + CurrentUpgradeLevel + 1, Owner.Creature, this);
+    }
+}
