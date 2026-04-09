fde9880bfc4ef4232278043695d502d026250c74
AquaMeow285714 <aquapatate@gmail.com>
2026-04-09 08:15:08 +0800

    修改三张牌，添加把共鸣放到词条最后的patch

--- src/DemoMod/TheGleaner/Cards/GleanerCard/Glissando.cs
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/Glissando.cs
@@ -14,59 +14,59 @@ namespace DemoMod.TheGleaner.Cards.GleanerCard;
 
 [Pool(typeof(CardPool))]
 public class Glissando : CustomCardModel {
-    public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+	public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
 
-    public override bool GainsBlock {
-        get {
-            return true;
-        }
-    }
+	public override bool GainsBlock {
+		get {
+			return true;
+		}
+	}
 
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new IntVar("Amount", 1),
-        new DamageVar(7, ValueProp.Move),
-        new BlockVar(7, ValueProp.Move),
-        new PowerVar<VulnerablePower>(1)
-    ];
+	protected override IEnumerable<DynamicVar> CanonicalVars => [
+		new IntVar("Amount", 1),
+		new DamageVar(7, ValueProp.Move),
+		new BlockVar(7, ValueProp.Move),
+		new PowerVar<VulnerablePower>(1)
+	];
 
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
-        HoverTipFactory.FromPower<WeakPower>(),
-        HoverTipFactory.FromPower<VulnerablePower>()
-    ];
+	protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+		HoverTipFactory.FromPower<WeakPower>(),
+		HoverTipFactory.FromPower<VulnerablePower>()
+	];
 
-    public Glissando() : base(1, CardType.Attack, CardRarity.Basic, TargetType.AllEnemies) {
-    }
+	public Glissando() : base(2, CardType.Attack, CardRarity.Basic, TargetType.AllEnemies) {
+	}
 
-    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
-        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay, false);
+	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+		await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay, false);
 
-        await CreatureCmd.TriggerAnim(Owner.Creature, "Attack", 0.5f);
+		await CreatureCmd.TriggerAnim(Owner.Creature, "Attack", 0.5f);
 
-        IEnumerable<DamageResult> damageResults =
-            await CreatureCmd.Damage(choiceContext, CombatState.HittableEnemies, DynamicVars.Damage, Owner.Creature, this);
+		IEnumerable<DamageResult> damageResults =
+			await CreatureCmd.Damage(choiceContext, CombatState.HittableEnemies, DynamicVars.Damage, Owner.Creature, this);
 
-        int count = damageResults.Count(result => result.WasTargetKilled);
+		int count = damageResults.Count(result => result.WasTargetKilled);
 
-        if (count == damageResults.Count() - 1) {
-            await PowerCmd.Apply<WeakPower>(
-                CombatState.HittableEnemies,
-                DynamicVars["Amount"].BaseValue,
-                Owner.Creature,
-                this
-            );
+		if (count == damageResults.Count() - 1) {
+			await PowerCmd.Apply<WeakPower>(
+				CombatState.HittableEnemies,
+				DynamicVars["Amount"].BaseValue,
+				Owner.Creature,
+				this
+			);
 
-            await PowerCmd.Apply<VulnerablePower>(
-                CombatState.HittableEnemies,
-                DynamicVars.Vulnerable.BaseValue,
-                Owner.Creature,
-                this,
-                false
-            );
-        }
-    }
+			await PowerCmd.Apply<VulnerablePower>(
+				CombatState.HittableEnemies,
+				DynamicVars.Vulnerable.BaseValue,
+				Owner.Creature,
+				this,
+				false
+			);
+		}
+	}
 
-    protected override void OnUpgrade() {
-        DynamicVars.Damage.UpgradeValueBy(3);
-        DynamicVars.Block.UpgradeValueBy(3);
-    }
+	protected override void OnUpgrade() {
+		DynamicVars.Damage.UpgradeValueBy(3);
+		DynamicVars.Block.UpgradeValueBy(3);
+	}
 }
--- src/DemoMod/TheGleaner/Cards/GleanerCard/StringAndPillar.cs
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/StringAndPillar.cs
@@ -19,7 +19,7 @@ public class StringAndPillar : CustomCardModel {
 	protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<StrengthPower>(), HoverTipFactory.FromPower<DexterityPower>()];
 	public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Innate, CardKeyword.Retain, CustomEnums.Resonance];
 
-	public StringAndPillar() : base(6, CardType.Power, CardRarity.Basic, TargetType.Self) {
+	public StringAndPillar() : base(5, CardType.Power, CardRarity.Basic, TargetType.Self) {
 	}
 
 	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
