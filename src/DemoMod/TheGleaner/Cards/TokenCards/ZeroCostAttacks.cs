141d0ba338f15eb77935cc7baa88fab7110b6b5b
tldyl <756560020@qq.com>
2026-04-27 18:40:22 +0800

    新增1张牌

--- src/DemoMod/TheGleaner/Cards/TokenCards/ZeroCostAttacks.cs
+++ src/DemoMod/TheGleaner/Cards/TokenCards/ZeroCostAttacks.cs
@@ -5,6 +5,7 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Factories;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
 
@@ -14,11 +15,16 @@ namespace DemoMod.TheGleaner.Cards.TokenCards;
 public class ZeroCostAttacks : CustomCardModel, IChoosable {
     public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
     public override bool CanBeGeneratedInCombat => false;
+    private List<DynamicVar> _dynamicVars = [];
 
     public ZeroCostAttacks() : base(-1, CardType.Skill, CardRarity.Token, TargetType.None) {
         
     }
 
+    public void addVar(DynamicVar dynamicVar) {
+        _dynamicVars.Add(dynamicVar);
+    }
+    
     public async Task OnChosen(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
         List<CardPoolModel> list1 = Owner.UnlockState.CharacterCardPools.ToList();
         if (list1.Count > 1) {
@@ -27,7 +33,7 @@ public class ZeroCostAttacks : CustomCardModel, IChoosable {
         IEnumerable<CardModel> cards = from c in list1.SelectMany(c => c.GetUnlockedCards(Owner.UnlockState, Owner.RunState.CardMultiplayerConstraint))
             where c.Type == CardType.Attack && c.EnergyCost.Canonical == 0
             select c;
-        List<CardModel> list2 = CardFactory.GetDistinctForCombat(Owner, cards, 3, Owner.RunState.Rng.CombatCardGeneration).ToList();
+        List<CardModel> list2 = CardFactory.GetDistinctForCombat(Owner, cards, _dynamicVars[0].IntValue, Owner.RunState.Rng.CombatCardGeneration).ToList();
         if (IsUpgraded) {
             foreach (CardModel card2 in list2) {
                 CardCmd.Upgrade(card2);
