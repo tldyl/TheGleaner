1164f132490a8c1d7516c8c0f7771778db330d42
tldyl <756560020@qq.com>
2026-04-27 12:24:25 +0800

    新增3张牌

--- src/DemoMod/TheGleaner/Cards/GleanerCard/improvisation.cs
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/improvisation.cs
@@ -7,8 +7,10 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
 
 
 namespace DemoMod.TheGleaner.Cards.GleanerCard;
@@ -18,6 +20,7 @@ public class Improvisation : CustomCardModel {
     public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
 
     protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(7, ValueProp.Move),
         new CardsVar(3)
     ];
 
@@ -47,13 +50,19 @@ public class Improvisation : CustomCardModel {
         }
     }
 
-    public Improvisation() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self) {
+    public Improvisation() : base(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy) {
     }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+        AttackCommand _ = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .Execute(choiceContext);
         CardModel chosenCard = await CardSelectCmd.FromChooseACardScreen(choiceContext, OptionCards, Owner);
         if (chosenCard != null) {
             await ((IChoosable) chosenCard).OnChosen(choiceContext, cardPlay);
         }
     }
+    
+    protected override void OnUpgrade() => DynamicVars.Damage.UpgradeValueBy(2);
 }
