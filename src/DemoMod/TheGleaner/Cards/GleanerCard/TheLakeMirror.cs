3b4675f0bfa5f714d3a94262417e8f53299dbe90
tldyl <756560020@qq.com>
2026-04-29 14:01:44 +0800

    湖中镜重做

--- src/DemoMod/TheGleaner/Cards/GleanerCard/TheLakeMirror.cs
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/TheLakeMirror.cs
@@ -1,5 +1,6 @@
 using BaseLib.Abstracts;
 using BaseLib.Utils;
+using DemoMod.TheGleaner.Enums;
 using DemoMod.TheGleaner.Pools;
 using DemoMod.TheGleaner.Powers;
 using MegaCrit.Sts2.Core.Commands;
@@ -13,31 +14,32 @@ namespace DemoMod.TheGleaner.Cards.GleanerCard;
 
 [Pool(typeof(CardPool))]
 public class TheLakeMirror : CustomCardModel {
-	public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+    public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
 
-	protected override IEnumerable<DynamicVar> CanonicalVars => [
-		new IntVar("Amount", 2)
-	];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new IntVar("Amount", 1)
+    ];
 
-	protected override IEnumerable<IHoverTip> ExtraHoverTips => [
-		HoverTipFactory.FromPower<StrengthPower>(),
-		HoverTipFactory.FromPower<DexterityPower>()
-	];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromPower<StrengthPower>(),
+        HoverTipFactory.FromPower<DexterityPower>(),
+        HoverTipFactory.FromKeyword(CustomEnums.Resonance)
+    ];
 
-	public TheLakeMirror() : base(1, CardType.Power, CardRarity.Uncommon, TargetType.Self) {
-	}
+    public TheLakeMirror() : base(1, CardType.Power, CardRarity.Uncommon, TargetType.Self) {
+    }
 
-	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
-		await PowerCmd.Apply<TheLakeMirrorPower>(
-			Owner.Creature,
-			DynamicVars["Amount"].BaseValue, // 👉 这里传“2”
-			Owner.Creature,
-			this,
-			false
-		);
-	}
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+        await PowerCmd.Apply<TheLakeMirrorPower>(
+            Owner.Creature,
+            DynamicVars["Amount"].BaseValue,
+            Owner.Creature,
+            this,
+            false
+        );
+    }
 
-	protected override void OnUpgrade() {
-		DynamicVars["Amount"].UpgradeValueBy(1);
-	}
+    protected override void OnUpgrade() {
+        DynamicVars["Amount"].UpgradeValueBy(1);
+    }
 }
--- src/DemoMod/TheGleaner/Powers/TheLakeMirrorPower.cs
+++ src/DemoMod/TheGleaner/Powers/TheLakeMirrorPower.cs
@@ -1,42 +1,30 @@
-using System.Threading.Tasks;
 using BaseLib.Abstracts;
+using DemoMod.TheGleaner.Enums;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.Models.Powers;
 
 namespace DemoMod.TheGleaner.Powers;
 
-public class TheLakeMirrorPower : CustomPowerModel
-{
+public class TheLakeMirrorPower : CustomPowerModel {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
 
-    public override async Task AfterCardPlayed(PlayerChoiceContext context, CardPlay cardPlay)
-    {
-        if (cardPlay.Card.Owner != Owner.Player)
-        {
+    public override async Task AfterCardPlayed(PlayerChoiceContext context, CardPlay cardPlay) {
+        if (cardPlay.Card.Owner != Owner.Player) {
             return;
         }
 
-        Flash();
-
-        // 攻击 -> 敏捷（临时）
-        if (cardPlay.Card.Type == CardType.Attack)
-        {
-            await PowerCmd.Apply<DemoTempDexterityPower>(
+        if (cardPlay.Card.Keywords.Contains(CustomEnums.Resonance)) {
+            Flash();
+            await PowerCmd.Apply<DemoTempStrengthPower>(
                 Owner,
                 Amount,
                 Owner,
                 null
             );
-        }
-
-        // 技能 -> 力量（临时）
-        if (cardPlay.Card.Type == CardType.Skill)
-        {
-            await PowerCmd.Apply<DemoTempStrengthPower>(
+            await PowerCmd.Apply<DemoTempDexterityPower>(
                 Owner,
                 Amount,
                 Owner,
