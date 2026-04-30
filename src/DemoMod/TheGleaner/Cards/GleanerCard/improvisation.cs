34995892f867ba7e1859cc5d45a0312af880e993
AquaMeow285714 <aquapatate@gmail.com>
2026-04-30 17:46:03 +0800

    Add files via upload

--- src/DemoMod/TheGleaner/Cards/GleanerCard/improvisation.cs
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/improvisation.cs
@@ -17,42 +17,39 @@ namespace DemoMod.TheGleaner.Cards.GleanerCard;
 
 [Pool(typeof(CardPool))]
 public class Improvisation : CustomCardModel {
-    public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+	public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
 
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new CardsVar(3)
-    ];
+	protected override IEnumerable<DynamicVar> CanonicalVars => [
+		new CardsVar(3)
+	];
 
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [
-        CardKeyword.Exhaust
-    ];
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromKeyword(CustomEnums.Score)];
-    private List<CardModel> OptionCards {
-        get {
-            List<CardModel> optionCards = [
-                ModelDb.Card<ZeroCostAttacks>().ToMutable(),
-                ModelDb.Card<OneCostAttacks>().ToMutable(),
-                ModelDb.Card<TwoCostAttacks>().ToMutable(),
-                ModelDb.Card<ThreeOrMoreCostAttacks>().ToMutable()
-            ];
-            foreach (CardModel card in optionCards) {
-                card.Owner = Owner;
-                card.DynamicVars.Cards.BaseValue = DynamicVars.Cards.BaseValue;
-                if (IsUpgraded) {
-                    CardCmd.Upgrade(card);
-                }
-            }
-            return optionCards;
-        }
-    }
+	protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromKeyword(CustomEnums.Score)];
+	private List<CardModel> OptionCards {
+		get {
+			List<CardModel> optionCards = [
+				ModelDb.Card<ZeroCostAttacks>().ToMutable(),
+				ModelDb.Card<OneCostAttacks>().ToMutable(),
+				ModelDb.Card<TwoCostAttacks>().ToMutable(),
+				ModelDb.Card<ThreeOrMoreCostAttacks>().ToMutable()
+			];
+			foreach (CardModel card in optionCards) {
+				card.Owner = Owner;
+				card.DynamicVars.Cards.BaseValue = DynamicVars.Cards.BaseValue;
+				if (IsUpgraded) {
+					CardCmd.Upgrade(card);
+				}
+			}
+			return optionCards;
+		}
+	}
 
-    public Improvisation() : base(1, CardType.Skill, CardRarity.Rare, TargetType.Self) {
-    }
+	public Improvisation() : base(1, CardType.Skill, CardRarity.Rare, TargetType.Self) {
+	}
 
-    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
-        CardModel chosenCard = await CardSelectCmd.FromChooseACardScreen(choiceContext, OptionCards, Owner);
-        if (chosenCard != null) {
-            await ((IChoosable) chosenCard).OnChosen(choiceContext, cardPlay, 3);
-        }
-    }
+	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+		CardModel chosenCard = await CardSelectCmd.FromChooseACardScreen(choiceContext, OptionCards, Owner);
+		if (chosenCard != null) {
+			await ((IChoosable) chosenCard).OnChosen(choiceContext, cardPlay, 3);
+		}
+	}
 }
