fc4843d0327b12ff4170e2b4e36497378dc9d326
AquaMeow285714 <aquapatate@gmail.com>
2026-05-02 19:12:23 +0800

    Add files via upload

--- src/DemoMod/TheGleaner/Cards/GleanerCard/CrescendoTempo.cs
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/CrescendoTempo.cs
@@ -15,45 +15,45 @@ namespace DemoMod.TheGleaner.Cards.GleanerCard;
 
 [Pool(typeof(CardPool))]
 public class CrescendoTempo : CustomCardModel {
-    //public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new DamageVar(10, ValueProp.Move),
-        new IntVar("Increase", 3),
-        new IntVar("Sum", 0),
-    ];
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [
-        CardKeyword.Ethereal
-    ];
+	public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+	protected override IEnumerable<DynamicVar> CanonicalVars => [
+		new DamageVar(10, ValueProp.Move),
+		new IntVar("Increase", 3),
+		new IntVar("Sum", 0),
+	];
+	public override IEnumerable<CardKeyword> CanonicalKeywords => [
+		CardKeyword.Ethereal
+	];
 
-    public CrescendoTempo() : base(2, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy) {
-        
-    }
+	public CrescendoTempo() : base(2, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy) {
+		
+	}
 
-    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
-        AttackCommand _ = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
-            .FromCard(this)
-            .Targeting(cardPlay.Target)
-            .Execute(choiceContext);
-    }
+	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+		AttackCommand _ = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+			.FromCard(this)
+			.Targeting(cardPlay.Target)
+			.Execute(choiceContext);
+	}
 
-    public override async Task BeforeTurnEnd(PlayerChoiceContext choiceContext, CombatSide side) {
-        if (side != CombatSide.Player) {
-            return;
-        }
-        DynamicVars["Sum"].BaseValue += DynamicVars["Increase"].BaseValue;
-    }
-    
-    public override Decimal ModifyDamageAdditive(
-        Creature? target,
-        Decimal amount,
-        ValueProp props,
-        Creature? dealer,
-        CardModel? cardSource) {
-        if (cardSource == this && !props.HasFlag(ValueProp.Unpowered)) {
-            return DynamicVars["Sum"].BaseValue;
-        }
-        return 0;
-    }
-    
-    protected override void OnUpgrade() => DynamicVars["Increase"].UpgradeValueBy(1);
+	public override async Task BeforeTurnEnd(PlayerChoiceContext choiceContext, CombatSide side) {
+		if (side != CombatSide.Player) {
+			return;
+		}
+		DynamicVars["Sum"].BaseValue += DynamicVars["Increase"].BaseValue;
+	}
+	
+	public override Decimal ModifyDamageAdditive(
+		Creature? target,
+		Decimal amount,
+		ValueProp props,
+		Creature? dealer,
+		CardModel? cardSource) {
+		if (cardSource == this && !props.HasFlag(ValueProp.Unpowered)) {
+			return DynamicVars["Sum"].BaseValue;
+		}
+		return 0;
+	}
+	
+	protected override void OnUpgrade() => DynamicVars["Increase"].UpgradeValueBy(1);
 }
