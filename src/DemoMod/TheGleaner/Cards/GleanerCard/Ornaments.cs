169adbd7596d353d45ea705f35a6416d30059832
AquaMeow285714 <aquapatate@gmail.com>
2026-05-05 20:44:01 +0800

    Add files via upload

--- src/DemoMod/TheGleaner/Cards/GleanerCard/Ornaments.cs
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/Ornaments.cs
@@ -14,28 +14,28 @@ namespace DemoMod.TheGleaner.Cards.GleanerCard;
 
 [Pool(typeof(CardPool))]
 public class Ornaments : CustomCardModel {
-    //public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+	public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
 	
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new PowerVar<VulnerablePower>(1),
-        new DamageVar("Damage1", 1, ValueProp.Move),
-        new DamageVar("Damage2", 4, ValueProp.Move)
-    ];
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
-        HoverTipFactory.FromPower<VulnerablePower>()
-    ];
+	protected override IEnumerable<DynamicVar> CanonicalVars => [
+		new PowerVar<VulnerablePower>(1),
+		new DamageVar("Damage1", 1, ValueProp.Move),
+		new DamageVar("Damage2", 4, ValueProp.Move)
+	];
+	protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+		HoverTipFactory.FromPower<VulnerablePower>()
+	];
 
-    public Ornaments() : base(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy) {
-        
-    }
+	public Ornaments() : base(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy) {
+		
+	}
 
-    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
-        await using AttackContext context = await AttackCommand.CreateContextAsync(Owner.Creature.CombatState, this);
-        await CreatureCmd.TriggerAnim(Owner.Creature, "Attack", 0.5f);
-        context.AddHit(await CreatureCmd.Damage(choiceContext, cardPlay.Target, (DamageVar) DynamicVars["Damage1"], this));
-        await PowerCmd.Apply<VulnerablePower>(cardPlay.Target, DynamicVars["VulnerablePower"].BaseValue, Owner.Creature, this);
-        context.AddHit(await CreatureCmd.Damage(choiceContext, cardPlay.Target, (DamageVar) DynamicVars["Damage2"], this));
-    }
+	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+		await using AttackContext context = await AttackCommand.CreateContextAsync(Owner.Creature.CombatState, this);
+		await CreatureCmd.TriggerAnim(Owner.Creature, "Attack", 0.5f);
+		context.AddHit(await CreatureCmd.Damage(choiceContext, cardPlay.Target, (DamageVar) DynamicVars["Damage1"], this));
+		await PowerCmd.Apply<VulnerablePower>(cardPlay.Target, DynamicVars["VulnerablePower"].BaseValue, Owner.Creature, this);
+		context.AddHit(await CreatureCmd.Damage(choiceContext, cardPlay.Target, (DamageVar) DynamicVars["Damage2"], this));
+	}
 
-    protected override void OnUpgrade() => DynamicVars["Damage2"].UpgradeValueBy(2);
+	protected override void OnUpgrade() => DynamicVars["Damage2"].UpgradeValueBy(2);
 }
