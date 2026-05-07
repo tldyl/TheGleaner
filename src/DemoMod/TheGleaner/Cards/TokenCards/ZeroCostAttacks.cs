31ced6de4377ea05f0c017c782a7c0b6afcc94a0
AquaMeow285714 <aquapatate@gmail.com>
2026-05-07 11:47:56 +0800

    纺声系列削弱

--- src/DemoMod/TheGleaner/Cards/TokenCards/ZeroCostAttacks.cs
+++ src/DemoMod/TheGleaner/Cards/TokenCards/ZeroCostAttacks.cs
@@ -13,37 +13,40 @@ namespace DemoMod.TheGleaner.Cards.TokenCards;
 
 [Pool(typeof(TokenCardPool))]
 public class ZeroCostAttacks : CustomCardModel, IChoosable {
-    public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
-    public override bool CanBeGeneratedInCombat => false;
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3)];
+	public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+	public override bool CanBeGeneratedInCombat => false;
+	protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3)];
 
-    public ZeroCostAttacks() : base(-1, CardType.Skill, CardRarity.Token, TargetType.None) {
-        
-    }
+	public ZeroCostAttacks() : base(-1, CardType.Skill, CardRarity.Token, TargetType.None) {
+		
+	}
 
-    public async Task OnChosen(PlayerChoiceContext choiceContext, CardPlay cardPlay, params object[] extraParams) {
-        int addAmount = 3;
-        if (extraParams is {Length: > 0}) {
-            addAmount = (int)extraParams[0];
-        }
-        List<CardPoolModel> list1 = Owner.UnlockState.CharacterCardPools.ToList();
-        if (list1.Count > 1) {
-            list1.Remove(Owner.Character.CardPool);
-        }
-        IEnumerable<CardModel> cards = from c in list1.SelectMany(c => c.GetUnlockedCards(Owner.UnlockState, Owner.RunState.CardMultiplayerConstraint))
-            where c.Type == CardType.Attack && c.EnergyCost.Canonical == 0 && !c.EnergyCost.CostsX && !c.HasStarCostX && !c.Tags.Contains(CardTag.OstyAttack)
-            select c;
-        List<CardModel> list2 = CardFactory.GetDistinctForCombat(Owner, cards, addAmount, Owner.RunState.Rng.CombatCardGeneration).ToList();
-        if (IsUpgraded) {
-            foreach (CardModel card2 in list2) {
-                CardCmd.Upgrade(card2);
-            }
-        }
-        foreach (CardModel c in list2) {
-            await ScorePileCmd.AddCards(Owner.PlayerCombatState, Owner, c);
-        }
-        foreach (CardModel c in list2) {
-            CardCmd.Preview(c);
-        }
-    }
+	public async Task OnChosen(PlayerChoiceContext choiceContext, CardPlay cardPlay, params object[] extraParams) {
+		int addAmount = 3;
+		if (extraParams is {Length: > 0}) {
+			addAmount = (int)extraParams[0];
+		}
+		List<CardPoolModel> list1 = Owner.UnlockState.CharacterCardPools.ToList();
+		if (list1.Count > 1) {
+			list1.Remove(Owner.Character.CardPool);
+		}
+		IEnumerable<CardModel> cards = from c in list1.SelectMany(c => c.GetUnlockedCards(Owner.UnlockState, Owner.RunState.CardMultiplayerConstraint))
+			where c.Type == CardType.Attack && c.EnergyCost.Canonical == 0 && !c.EnergyCost.CostsX && !c.HasStarCostX && !c.Tags.Contains(CardTag.OstyAttack)
+			select c;
+		List<CardModel> list2 = CardFactory.GetDistinctForCombat(Owner, cards, addAmount, Owner.RunState.Rng.CombatCardGeneration).ToList();
+		if (IsUpgraded) {
+			foreach (CardModel card2 in list2) {
+				CardCmd.Upgrade(card2);
+			}
+		}
+		foreach (CardModel c in list2) {
+			await ScorePileCmd.AddCards(Owner.PlayerCombatState, Owner, c);
+		}
+		foreach (CardModel c in list2) {
+				if (!c.Keywords.Contains(CardKeyword.Ethereal)) {
+				c.AddKeyword(CardKeyword.Ethereal);
+			}
+			CardCmd.Preview(c);
+		}
+	}
 }
