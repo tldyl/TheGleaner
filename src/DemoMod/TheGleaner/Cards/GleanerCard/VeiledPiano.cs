b83ca271efeebdac1bcea90c473c477351df692d
AquaMeow285714 <aquapatate@gmail.com>
2026-05-15 02:18:09 +0800

    新雾中的钢琴

---
+++ src/DemoMod/TheGleaner/Cards/GleanerCard/VeiledPiano.cs
@@ -0,0 +1,58 @@
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using DemoMod.TheGleaner.Enums;
+using DemoMod.TheGleaner.Pools;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace DemoMod.TheGleaner.Cards.GleanerCard;
+
+[Pool(typeof(CardPool))]
+public class VeiledPiano : CustomCardModel, IConcertoCard
+{
+	public override string PortraitPath => $"res://TheGleaner/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+	public override bool GainsBlock => true;
+	protected override IEnumerable<DynamicVar> CanonicalVars => [
+		new BlockVar(7, ValueProp.Move),
+		new IntVar("VigorVal", 3),
+		new IntVar("VulVal", 1)
+	];
+
+	protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+		HoverTipFactory.FromKeyword(CustomEnums.Concerto),
+		HoverTipFactory.FromPower<VulnerablePower>(),
+		HoverTipFactory.FromPower<VigorPower>()
+	];
+
+	public VeiledPiano() : base(2, CardType.Skill, CardRarity.Common, TargetType.Self)
+	{
+	}
+
+	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+	{
+		await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+		await PowerCmd.Apply<VigorPower>(Owner.Creature, DynamicVars["VigorVal"].BaseValue, Owner.Creature, this);
+	}
+
+	protected override void OnUpgrade()
+	{
+		DynamicVars.Block.UpgradeValueBy(2);
+		DynamicVars["VigorVal"].UpgradeValueBy(1);
+	}
+
+	public async Task OnConcerto(CombatState combatState, PlayerChoiceContext choiceContext, CardPlay cardPlay)
+	{
+		await PowerCmd.Apply<VulnerablePower>(
+			Owner.Creature.CombatState.HittableEnemies,
+			DynamicVars["VulVal"].BaseValue,
+			Owner.Creature,
+			this
+		);
+	}
+}
