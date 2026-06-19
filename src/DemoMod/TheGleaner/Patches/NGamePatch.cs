fe1697442fdebd4b137700156fd6ef305b731e3b
tldyl <756560020@qq.com>
2026-06-19 14:56:38 +0800

    更健康的实现音频播放器的初始化

--- src/DemoMod/TheGleaner/Patches/NGamePatch.cs
+++ src/DemoMod/TheGleaner/Patches/NGamePatch.cs
@@ -3,15 +3,17 @@ using DemoMod.TheGleaner.Utils;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Assets;
 using MegaCrit.Sts2.Core.Debug;
+using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Nodes;
 
 namespace TheGleaner.DemoMod.TheGleaner.Patches;
 
 public class NGamePatch {
-    [HarmonyPatch(typeof(NGame), "_EnterTree")]
+    [HarmonyPatch(typeof(OneTimeInitialization), "ExecuteEssential")]
     public static class PatchReady {
-        public static void Prefix(NGame __instance) {
+        public static void Prefix() {
+            NGame __instance = NGame.Instance;
             Log.Info("Initializing SoundManager and NGrayGradientVfxPostProcessor");
             __instance.AddChild(new SoundManager());
             SoundKeys.Initialize();
--- src/DemoMod/TheGleaner/Patches/SfxCmdPatch.cs
+++ src/DemoMod/TheGleaner/Patches/SfxCmdPatch.cs
@@ -1,10 +1,6 @@
-using DemoMod.TheGleaner.Nodes.Vfx;
 using DemoMod.TheGleaner.Utils;
 using HarmonyLib;
-using MegaCrit.Sts2.Core.Assets;
 using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Logging;
-using MegaCrit.Sts2.Core.Nodes;
 
 namespace TheGleaner.DemoMod.TheGleaner.Patches;
 
@@ -13,14 +9,6 @@ public class SfxCmdPatch {
     public static class PatchPlay1 {
         public static bool Prefix(string sfx, float volume) {
             if (!sfx.StartsWith("event:")) {
-                if (SoundManager.Instance == null) {
-                    Log.Info("Initializing SoundManager and NGrayGradientVfxPostProcessor");
-                    NGame.Instance.AddChild(new SoundManager());
-                    SoundKeys.Initialize();
-
-                    NGrayGradientVfxPostProcessor postProcessor = PreloadManager.Cache.GetScene("res://TheGleaner/scenes/vfx/gray_gradient_vfx.tscn").Instantiate<NGrayGradientVfxPostProcessor>();
-                    NGame.Instance.AddChild(postProcessor);
-                }
                 SoundManager.Instance.PlaySound(sfx, volume);
                 return false;
             }
@@ -32,14 +20,6 @@ public class SfxCmdPatch {
     public static class PatchPlay2 {
         public static bool Prefix(string sfx, string param , float val, float volume) {
             if (!sfx.StartsWith("event:")) {
-                if (SoundManager.Instance == null) {
-                    Log.Info("Initializing SoundManager and NGrayGradientVfxPostProcessor");
-                    NGame.Instance.AddChild(new SoundManager());
-                    SoundKeys.Initialize();
-
-                    NGrayGradientVfxPostProcessor postProcessor = PreloadManager.Cache.GetScene("res://TheGleaner/scenes/vfx/gray_gradient_vfx.tscn").Instantiate<NGrayGradientVfxPostProcessor>();
-                    NGame.Instance.AddChild(postProcessor);
-                }
                 SoundManager.Instance.PlaySound(sfx, volume);
                 return false;
             }
